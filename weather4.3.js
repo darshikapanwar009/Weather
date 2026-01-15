ed2df9a50245ec0e3caad2c71810a2f7fa524904
darshikapanwar009 <darshikapanwar1002@gmail.com>
2026-01-15 15:13:50 +0530

    weather app

---
+++ weather4.3.js
@@ -0,0 +1,38 @@
+     const inpt=document.getElementById("inp");
+     const btn=document.getElementById("search-btn");
+     const API_KEY="f5d9f9bd6596e4718945a0a3cacc30c8";
+     btn.addEventListener("click",()=>{
+     if(inpt.value==""){
+        alert("First enter the city name");
+     }
+     else{
+      getweatherData();
+     }
+     })
+     async function getweatherData(){
+     try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inpt.value}&appid=${API_KEY}&units=metric`
+    );
+     let data=await response.json();
+     /*error handling ffor wrong city name*/
+     if (data.cod === "404") {
+  document.getElementById("result").innerHTML = `
+    <p style="color:red;">City not found. Please enter a valid city name.</p>
+  `;
+  return;
+}
+ /*displaying the weather parameters*/
+     document.getElementById("result").innerHTML = `
+      <h2>${data.name}</h2>
+      <p>Temperature: ${data.main.temp} °C</p>
+      <p>Weather: ${data.weather[0].description}</p>
+      <p>Humidity:${data.main.humidity}%</p>
+    `;
+     }
+    catch (error) {
+        console.log("Error fetching weather:", error);
+    }
+    console.log(inpt.value);
+    }
+
