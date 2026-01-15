5685e911561e3fcaaeda63f4d4fb460589fb780e
jainpalash30 <jainpalash3011@gmail.com>
2026-01-15 16:29:26 +0530

    factorial solved

--- Rajeev-script.js
+++ Rajeev-script.js
@@ -2,3 +2,14 @@
 let a=1;
 let b=2;
 console.log(a);
+
+function fac(num){
+    if(num==0 || num==1){
+        return 1;
+    }    else{
+        return num*fac(num-1);
+    }       
+    
+}
+
+fac(5);
